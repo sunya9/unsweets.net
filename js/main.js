cb236386557e134cb548b82f90a652890a55d02a
sunya <mugen.Xyz@gmail.com>
2014-11-02 23:48:52 +0900

    Refactoring(Improve js).

--- js/main.js
+++ js/main.js
@@ -1,243 +1,255 @@
 $(function() {
-    $window = $(window);
-    $window.resize(function() {
-        centering();
-        fitDiv();
-    });
-    $window.resize();
-    var $fixedNavAnchor = $("div#fixed-nav>div.inner>ul>li>a");
-    var updatePosition = function() {
-        var ret = _.object($fixedNavAnchor.map(function(e) {
-            return [[this.hash, [$(this.hash).offset().top, $(this.hash).outerHeight() - 1]]];
-        }).get());
-        return {
-            keys: _.keys(ret),
-            values: _.values(ret)
-        };
+    var SCROLL_SNAP_DELAY = 1000;
+    var UPDATE_NAV_DELAY = 100;
+    var RESIZE_DELAY = 500;
+    var TRIGGER_OFFSET = 7 / 8;
+    var CONTENT_TRANSLATE_DISTANCE = 100;
+    var SLIDE = {
+        DURATION: 1000,
+        DELAY: 500,
+        EASING: "easeOutCubic"
     };
+    var SCROLL_OPTION = {
 
-    //detect language
-    var language = (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0,2);
-    var paramHash = {};
-    var paramArray = location.search.substring(1).split("&");
-    for (var i = 0; i < paramArray.length; i++) {
-        var kv = paramArray[i].split("=");
-        paramHash[kv[0]] = kv[1];
+        duration: "normal",
+        easing: "easeOutCubic",
     };
-    
-    if(language == "en" || paramHash.la == "en"){
-        $("body *[data-lang-en]").each(function(){
-            $this = $(this);
-            var enStr = $this.data("lang-en");
-            $this.parent().css({fontFamily:"Roboto", fontWeight:300, fontSize:"110%"});
-            $this.html(enStr);
+    var LEFT = -1;
+    var RIGHT = 1;
+
+    var $window = $(window);
+    var $fixedNavAnchor = $("div#fixed-nav>div.inner>ul>li>a");
+    var $fixedNavList = $fixedNavAnchor.parent();
+    var $eachContent = $("div#contents>section");
+    var $footer = $("footer#footer");
+
+    init();
+
+    function init() {
+        $("div.left, div.right", $eachContent).addClass("invisible");
+
+        $eachContent.each(function() {
+            var $this = $(this);
+            $this.waypoint({
+                triggerOnce: true,
+                handler: function(direction) {
+                    if (direction == "down")
+                        $("div.left, div.right", $this).animate({
+                            opacity: 1
+                        }, {
+                            easing: "easeOutCubic",
+                            duration: 1000
+                        });
+                },
+                offset: $window.height() * TRIGGER_OFFSET
+            });
+        });
+
+        fitContents();
+        setupFixedNav();
+        setupEachMenu();
+
+        var scrollSnap = _.debounce(_scrollSnap, SCROLL_SNAP_DELAY);
+        var updateNavColor = _.debounce(_updateNavColor, UPDATE_NAV_DELAY);
+
+        $window.resize(_.debounce(function() {
+            centering();
+            fitContents();
+            scrollSnap();
+            console.log("resize");
+        }, RESIZE_DELAY));
+        $window.resize();
+
+        $window.scroll(function() {
+            if ($(this).is(":animated")) return;
+            updateNavColor();
+            scrollSnap();
+        });
+
+        // other setup
+
+        $("nav>ul>li>a").add($fixedNavAnchor).click(function() {
+            var hash = this.hash;
+            scrollTo(hash);
+            $fixedNavAnchor.removeClass("select");
+            $("a[href='" + hash + "']", "div#fixed-nav>div.inner>ul>li").addClass("select");
+            return false;
+        });
+
+        $("a#scroll_down").click(function(e) {
+            e.preventDefault();
+            scrollTo("#works");
+            return false;
+        });
+
+        $("#copyright a[href='#me']").click(function(e) {
+            e.preventDefault();
+            $("div.menu>ul>li>a[href='#me']", "section#about").click();
+            return false;
+        });
+
+        $(window).keydown(function(e) {
+            switch (e.keyCode) {
+                case 37:
+                    $("div#nav-prev>a").click();
+                    break;
+                case 39:
+                    $("div#nav-next>a").click();
+                    break;
+            }
         });
     }
-    var positionHash = _.once(updatePosition);
-    $("nav>ul>li>a").each(function(i, e) {
-        $("div.left-wrapper>div, div.right-wrapper>div", e.hash).css({
-            opacity: 0
-        });
-        $(e.hash).waypoint({
-            triggerOnce: true,
-            handler: function(direction) {
-                if (direction == "down")
-                    $("div.left-wrapper>div:visible, div.right-wrapper>div:visible", e.hash).animate({
-                        opacity: 1
-                    }, {
-                        easing: "easeOutCubic",
-                        duration: 1000
-                    });
-            },
-            offset: $window.height() * 7 / 8
-        });
-    });
 
-    function nearContent() {
-        var st = $window.scrollTop();
-        var wh = $window.innerHeight();
-        var sb = st + wh;
-        var $f = $("#footer");
-        var ft = $f.position().top;
-        var fh = $f.outerHeight();
-        if (sb >= (ft + fh / 2))
-            return "#footer";
-        var diffArr = _.map(positionHash().values, function(v, i) {
-            return _.min([Math.abs(st - (v[1] - wh) - v[0]), Math.abs(st - v[0])]);
-        });
-        var min = _.min(diffArr);
-        var minIndex = _.indexOf(diffArr, min);
-        return positionHash().keys[minIndex];
-    };
+    function getContentPosition() {
+        return $fixedNavAnchor.map(function() {
+            var $hash = $(this.hash);
+            var obj = (function(self) {
+                var top = $hash.offset().top;
+                return {
+                    anchor: self.hash,
+                    top: top,
+                    bottom: top + $hash.outerHeight() - 1
+                };
+            }(this));
+            return obj;
+        }).get();
+    }
 
-    var scrollOptions = {
-        duration: "normal",
-        easing: "easeOutCubic"
-    };
-    var calcElementPosition = _.debounce(function() {
-        var wh = $window.height();
-        var st = $window.scrollTop();
-        var sb = wh + st;
-        var hash = nearContent();
-        var ch = $(hash).outerHeight();
-        // console.log(hash);
-        var top = $(hash).position().top;
-        if (st != top) {
-            if (ch <= wh) {
-                $.scrollTo(hash, scrollOptions);
-            } else if (ch > wh) {
-                if (top < st && sb < top + ch) {} else if (st > top) {
-                    var pos = top + ch - wh;
-                    $.scrollTo(pos, scrollOptions);
-                } else if (ch < st) {
-                    $.scrollTo(hash, scrollOptions);
-                }
+    function setupFixedNav() {
+        _updateNavColor();
+        $fixedNavAnchor.each(function() {
+            $(this).append("<span>" + $(this).data("title") + "</span>");
+        });
+        $("div#nav-prev>a").click(function(e) {
+            e.preventDefault();
+            $fixedNavList.has(".select").prev().find("a").click();
+            return false;
+        });
+        $("div#nav-next>a").click(function(e) {
+            e.preventDefault();
+            $fixedNavList.has(".select").next().find("a").click();
+            return false;
+        });
+    }
+
+    function _scrollSnap() {
+        var content = getNearestContent();
+        var scrollTop = $window.scrollTop();
+        var windowHeight = $window.height();
+        var isLongContent = windowHeight < content.bottom - content.top;
+        if (isLongContent) {
+            if (scrollTop < content.top) {
+                scrollTo(content.anchor);
+            } else if (scrollTop + windowHeight > content.bottom) {
+                var pos = content.bottom - windowHeight;
+                scrollTo(pos);
             }
+        } else {
+            scrollTo(content.anchor);
         }
-    }, 1 * 1000);
-    var updateFixedNavColor = _.debounce(function() {
-        var hash = nearContent();
-        var $selectHref = $("a[href='" + hash + "']", "div#fixed-nav>div.inner>ul>li");
-        // var prevHash = $("div#fixed-nav>div.inner>ul>li>a.select").hash;
+    }
+
+    function fitContents() {
+        var windowHeight = $window.height();
+        $eachContent.each(function() {
+            var $contents = $("div.right>section", this);
+            var max = $(_.max($contents, function(content) {
+                return $(content).outerHeight();
+            })).height();
+            var windowHeight = $window.innerHeight();
+            max = max > windowHeight ? max : windowHeight;
+            var $bothBlock = $("div.left-wrapper, div.right-wrapper", this);
+            $bothBlock.css({
+                minHeight: max
+            });
+            var height = _.max($bothBlock.map(function() {
+                return $(this).height();
+            }));
+            height > max && $bothBlock.css({
+                minHeight: height
+            });
+        });
+    }
+
+    function scrollTo(name) {
+        $.scrollTo(name, SCROLL_OPTION);
+    }
+
+    function getNearestContent() {
+        var bottom = $window.scrollTop() + $window.height();
+        var middle = $window.scrollTop() + $window.height() / 2;
+        var positions = getContentPosition();
+        if (bottom > $footer.offset().top + $footer.height() / 2)
+            return _.last(positions);
+
+        var nearest = _.min(positions, function(pos) {
+            return Math.min(Math.abs(middle - pos.top), Math.abs(middle - pos.bottom));
+        });
+        return nearest;
+    };
+
+    function _updateNavColor() {
+        var $selectHref = $fixedNavAnchor.filter("[href='" + getNearestContent().anchor + "']");
         if (!$selectHref.hasClass("select")) {
-            // if (hash !== prevHash) {
             $fixedNavAnchor.removeClass("select");
             $selectHref.addClass("select").addClass("big-anim");
-            setTimeout(function() {
+            _.delay(function() {
                 $selectHref.removeClass("big-anim");
             }, 300);
         }
-    }, 100);
-    $window.scroll(calcElementPosition);
-    $window.resize(calcElementPosition);
-    $window.resize(_.debounce(function() {
-        positionHash = updatePosition();
-    }, 500));
-    $window.scroll(updateFixedNavColor);
-    // $window.scroll(_.throttle(function(){
-    //     var st = $window.scrollTop();
-    //     if(0 < st && st < $("header#header").height())
-    //     $("header#header").css({marginBottom:-st * 0.5, paddingTop:st  * 0.5});
-    // }, 100));
-    $("div.menu>ul>li>a").click(function() {
-        var $id = $(this.hash);
+    }
 
-        $(this);
-        var $section = $(this).parents("section");
-        var hash = "#" + $section.attr("id");
-        $.scrollTo(hash, scrollOptions);
-        var current = $section.find("div.right-wrapper > div.right > section:visible");
-        if ($(current).attr("id") == this.hash.substring(1)) return false;
-        var prevIndex = index = -1;
-        var $li = $(this).parent("li");
-        $li.siblings().addBack().each(function(i, e) {
-            prevIndex = $(e).hasClass("select") ? i : -1;
-            if (prevIndex >= 0) return false;
-        });
-        index = $(this).parents("ul").find("li>a").index(this);
-        var direction = prevIndex < index ? -1 : 1;
-        var distance = 100 * direction;
-        $li.siblings().removeClass("select").end().addClass("select");
-        var time = 500;
-        var easing = "easeOutCubic";
-        current.stop(true, false).css({
-            position: "absolute",
-            zIndex: "0"
-        }).animate({
-            left: distance,
-            opacity: 0
-        }, time * 2, easing, function() {
-            $(this).css({
+    function setupEachMenu() {
+        $("div.menu>ul>li>a").click(function() {
+            var $id = $(this.hash);
+
+            var $section = $(this).parents("section");
+            scrollTo("#" + $section.attr("id"));
+            var $currentContent = $("div.right > section:visible", $section);
+            if ($currentContent.attr("id") === this.hash.substring(1)) return false;
+            var $li = $(this).parent("li");
+            var $siblings = $li.siblings();
+            var $menuItem = $siblings.addBack();
+            var index = $li.index();
+            var prevIndex = $menuItem.filter(".select").index();
+            var direction = prevIndex < index ? RIGHT : LEFT;
+            var vector = CONTENT_TRANSLATE_DISTANCE * direction;
+            $menuItem.eq(prevIndex).removeClass("select");
+            $li.addClass("select");
+
+            $currentContent.stop(true, false).css({
+                position: "absolute",
+            }).animate({
+                left: -vector,
+                opacity: 0
+            }, SLIDE.DURATION, SLIDE.EASING, function() {
+                $(this).css({
+                    left: 0,
+                    display: "none",
+                    position: "static",
+                    opacity: 1
+                });
+            });
+            $id.stop(true, false).delay(SLIDE.DELAY).css({
+                position: "absolute",
+                left: vector,
+                display: "block",
+                opacity: 0
+            }).animate({
                 left: 0,
-                display: "none",
-                position: "static",
                 opacity: 1
+            }, SLIDE.DURATION, SLIDE.EASING, function() {
+                $(this).css({
+                    position: "static"
+                });
             });
+            return false;
         });
-        $id.stop(true, false).delay(time).css({
-            position: "absolute",
-            left: -distance,
-            display: "block",
-            opacity: 0
-        }).animate({
-            left: 0,
-            opacity: 1
-        }, time * 2, easing, function() {
-            $(this).css({
-                position: "static"
-            });
+    }
+
+    function centering() {
+        $("header#header").css({
+            height: $window.height()
         });
-        return false;
-    });
-    $("section.hide").hide();
-    $("nav>ul>li>a, div#fixed-nav>div.inner>ul>li>a").click(function() {
-        var hash = this.hash;
-        $.scrollTo(hash, scrollOptions);
-        $fixedNavAnchor.removeClass("select");
-        $("a[href='" + hash + "']", "div#fixed-nav>div.inner>ul>li").addClass("select");
-        return false;
-    });
-    $("div#nav-prev>a").click(function() {
-        var index = $(".select").index("div#fixed-nav>div.inner>ul>li>a") - 1;
-        if (index >= 0)
-            $fixedNavAnchor.eq(index).click();
-        return false;
-    });
-    $(window).keydown(function(e) {
-        switch (e.keyCode) {
-            case 37:
-                $("div#nav-prev>a").click();
-                break;
-            case 39:
-                $("div#nav-next>a").click();
-                break;
-        }
-    });
-    $("div#nav-next>a").click(function() {
-        var index = $(".select").index("div#fixed-nav>div.inner>ul>li>a") + 1;
-        if (index <= $fixedNavAnchor.length)
-            $fixedNavAnchor.eq(index).click();
-        return false;
-    });
-    $("#copyright a[href='#me']").click(function(e){
-        $("div.menu>ul>li>a[href='#me']").click();
-        return false;
-    });
-    // $("<img>").attr("src", "./images/avatar2.png");
-    // $("section#me img.avatar").hover(function() {
-    //     var s = $(this).attr("src");
-    //     $(this).attr("src", "./images/avatar2.png");
-    // }, function() {
-    //     $(this).attr("src", "./images/avatar.png");
-    // });
-    $fixedNavAnchor.each(function() {
-        $(this).append("<span>" + $(this).data("title") + "</span>");
-    });
+    }
 });
-
-function centering() {
-    $("header#header").css({
-        height: $window.innerHeight()
-    });
-}
-
-function fitDiv() {
-    $("nav>ul>li>a").each(function() {
-        var id = this.hash;
-        var heightArray = new Array();
-        $("div.right-wrapper>div.right>section", id).each(function() {
-            heightArray.push($(this).outerHeight());
-        });
-        var max = Math.max(heightArray);
-        var windowHeight = $window.innerHeight();
-        if (max > windowHeight) {
-            $(id).css({
-                height: max + 100
-            });
-        } else {
-            $(id).css({
-                height: windowHeight
-            });
-        }
-    });
-}
