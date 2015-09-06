e64f6ec16442fa48d2275b0bd4e21fc275120be3
sunya <mugen.Xyz@gmail.com>
2015-09-07 01:00:25 +0900

    remove unused tasks and refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,36 @@
 var gulp = require('gulp');
-var inject = require('gulp-inject');
-var watch = require('gulp-watch');
-var sass = require('gulp-ruby-sass');
-var plumber = require('gulp-plumber');
-var minifyCSS = require('gulp-minify-css');
-var mainBowerFiles = require('main-bower-files');
-var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
-var uglify = require('gulp-uglify');
-var es = require('event-stream');
-var stripDebug = require('gulp-strip-debug');
-var minifyHTML = require('gulp-minify-html');
-var livereload = require('gulp-livereload');
-var webserver = require('gulp-webserver');
-var path = require('path');
-
-var config = {
-  path: {
-    build_path: "./build/",
-    original: {
-      root: './',
-      css: './css/',
-      js: './js/',
-      js_components: './js/components/',
-      images: './images/',
-    },
-    build: (function() {
-      var res = {};
-      for (var key in this.original) {
-        res[key] = this.build_path + this.original[key];
-      }
-      return res;
-    })
+var $$ = require('gulp-load-plugins')({
+  rename: {
+    'gulp-ruby-sass': 'sass'
   }
-};
+});
+
+const HTML_FILES = [
+  './**/*.html',
+  '!./dist/**/*',
+  '!./node_modules/**/*',
+  '!./bower_components/**/*'
+];
 
 gulp.task('default', function() {
 
 });
 
-gulp.task('server', function(){
-  return gulp.src('.')
-  .pipe(webserver({
+gulp.task('server', ['watch'], function(){
+  return gulp.src('./dist')
+  .pipe($$.webserver({
     livereload: false,
-    directoryListing: true,
     open: true,
-    fallback: 'index.html'
   }));
 });
 
 gulp.task('watch', function() {
-  livereload.listen();
+  $$.livereload.listen();
   gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./bower_components/**/*', ['bower', 'inject']);
-});
-
-// copy bower components to build folder.
-gulp.task('bower', function() {
-  return gulp.src(mainBowerFiles({
-      paths: config.path.original.root
-    }))
-    .pipe(gulp.dest(config.path.original.js_components))
-    .pipe(gulp.dest(config.path.build().js_components));
+  gulp.watch(['./js/**/*.js' ,'./bower_components/**/*'], ['compress-js']);
+  gulp.watch('index.html', ['compress-html']);
+  return gulp.watch('./images/**/*', ['compress-image']);
 });
 
 // copy other files to build folder.
@@ -71,112 +40,80 @@ gulp.task('copy', function() {
     'humans.txt',
     '.htaccess',
   ]).
-  pipe(gulp.dest(config.path.build().root));
+  pipe(gulp.dest('./dist'));
 });
 
-// comporess images  and copy to build folder.
-gulp.task('image-compress', function() {
-  return gulp.src(config.path.original.images + '*')
-    .pipe(imagemin({
+// comporess images and copy to build folder.
+gulp.task('compress-image', function() {
+  return gulp.src('./images/*')
+    .pipe($$.imagemin({
       progressive: true,
       svgoPlugins: [{
         removeViewBox: false
       }],
       use: [pngquant()]
-    })).pipe(gulp.dest(config.path.build().images));
+    })).pipe(gulp.dest('./dist/images'));
 });
 
 // compile sass files.
 gulp.task('sass', function() {
   return gulp.src([
       './sass/**/*.scss',
-      '!./sass/components/**/*'
     ])
-    .pipe(plumber())
-    .pipe(sass({
+    .pipe($$.plumber())
+    .pipe($$.sass({
       style: 'compact',
       compass: true
     }))
-    .pipe(plumber.stop())
-    .pipe(minifyCSS())
-    .pipe(gulp.dest(config.path.build().css))
-    .pipe(gulp.dest(config.path.original.css))
-    .pipe(livereload());
+    .pipe($$.plumber.stop())
+    .pipe($$.minifyCss())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe($$.livereload());
 });
 
-// compress js files and copy to build folder.
-gulp.task('js-compress', function() {
-  return gulp.src([
-      config.path.original.js + '**/*.js',
-      '!' + config.path.original.js_components + '**/*.js',
-    ])
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest(config.path.build().js));
+// concat compoonents and compress js
+gulp.task('compress-js', function() {
+  return gulp.src('./bower.json')
+    .pipe($$.mainBowerFiles())
+    .pipe($$.addSrc('./js/**/*.js'))
+    .pipe($$.concat('all.js'))
+    .pipe($$.stripDebug())
+    .pipe($$.uglify({preserveComments: 'some'}))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe($$.livereload());
 });
 
 // compress html files
-gulp.task('html-compress', function() {
+gulp.task('compress-html', function() {
   var opts = {
     comments: true,
     spare: true,
     conditionals : true
   };
 
-  return gulp.src([
-      config.path.original.root + '**/*.html',
-      '!' + config.path.build().root + '**/*.html',
-      '!' + config.path.original.root + 'node_modules/**/*.html',
-      '!' + config.path.original.root + 'bower_components/**/*.html',
-    ])
-    .pipe(minifyHTML(opts))
-    .pipe(gulp.dest(config.path.build().root));
+  return gulp.src(HTML_FILES)
+    .pipe($$.minifyHtml(opts))
+    .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('compress', ['compress-html', 'compress-js', 'compress-image']);
+
 // inject 
-gulp.task('inject', function() {
-  var bowerFilesStream = gulp.src(mainBowerFiles().map(function(filePath) {
-    return config.path.original.js_components + path.basename(filePath);
-  }), {
-    read: false
-  });
-  var otherJsFilesStream = gulp.src([
-    config.path.original.js + '**/*.js',
-    config.path.original.css + 'main.css',
-    '!' + config.path.original.js + 'main.js',
-    '!' + config.path.original.js_components + '**/*.js'
-  ], {
-    read: false
-  });
-  return gulp.src('./index.html')
-    // .pipe(inject(gulp.src([config.path.original.js_components + 'modernizr.js'], {
-    //   read: false
-    // }), {
-    //   name: 'head',
-    //   relative: true
-    // }))
-    .pipe(inject(es.merge(bowerFilesStream, otherJsFilesStream), {
-      relative: true
-    }))
-    .pipe(inject(gulp.src(config.path.original.js + 'main.js', {
-      read: false
-    }), {
-      name: 'main',
-      relative: true
-    }))
-    .pipe(gulp.dest(config.path.original.root));
-    // .pipe(gulp.dest(config.path.build().root));
+gulp.task('inject', function(){
+  var target = HTML_FILES;
+  var sources = gulp.src(['./dist/js/*.js', './dist/css/*.css'], {read: false});
+  return gulp.src(HTML_FILES)
+    .pipe($$.inject(sources, {ignorePath: 'dist'}))
+    .pipe(gulp.dest('./dist'))
+    .pipe($$.livereload());
 });
 
 // exec build.
 gulp.task('build', [
-  'bower',
   'sass',
-  'image-compress',
-  'js-compress',
   'copy',
   'inject',
-  'html-compress'
+  'compress'
 ], function() {
   console.log('Finished build.');
 });
