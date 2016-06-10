92ac563fc1e106612672bd0d59de17fee04466ba
sunya <mugen.xyz@gmail.com>
2016-06-11 02:46:35 +0900

    Fix webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,20 @@ const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-let extractSass = new ExtractTextPlugin('css/main.css');
-
 const config = {
   entry: {
-    'js/main': './js/main'
+    'js/main.js': './js/main',
+    'js/sudoku-solver/main.js': './js/sudoku-solver/main',
+    'css/main': './sass/main.scss',
+    'css/sudoku-solver/main': './sass/sudoku-solver/main.scss'
   },
   output: {
     path: path.join(__dirname, '/build'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name]'
   },
   plugins: [
-    extractSass,
+    new ExtractTextPlugin('[name].css'),
     new webpack.ProvidePlugin({
       $: 'jquery'
     })
@@ -29,7 +30,7 @@ const config = {
         loader: 'babel'
       }, {
         test: /\.scss$/,
-        loader: extractSass.extract('css!postcss!sass')
+        loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
       }, {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf)$/,
         loader: 'url-loader?limit=100000'
