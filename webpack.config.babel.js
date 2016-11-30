efa89b6409240bb6ad60cbccf527a4adf7078c61
sunya <mugen.xyz@gmail.com>
2016-11-30 15:04:05 +0900

    Rename webpack.config.js to webpack.config.babel.js

---
+++ webpack.config.babel.js
@@ -0,0 +1,40 @@
+import webpack from 'webpack'
+import path from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+const extractCSS = new ExtractTextPlugin('[name]');
+
+const config = {
+  entry: './js/main',
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'js/[name].js'
+  },
+  plugins: [
+    extractCSS,
+    new webpack.ProvidePlugin({
+      $: 'jquery'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }, {
+        test: /\.scss$/,
+        loader: extractCSS.extract(['css', 'postcss', 'sass'])
+      }, {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  postcss : [
+    require('autoprefixer')
+  ]
+};
+
+export default config
