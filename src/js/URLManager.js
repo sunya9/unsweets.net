4530f6e67624a3133cc67f79c5b137fa3738adf6
sunya9 <mugen.xyz@gmail.com>
2017-10-06 02:31:42 +0900

    Remove hash in top

--- src/js/URLManager.js
+++ src/js/URLManager.js
@@ -1,20 +1,26 @@
 import debounce from 'lodash.debounce'
-import { scrollTo } from './util'
+import {
+  scrollTo
+} from './util'
 
 class URLManager {
   constructor(menu) {
-    if(!('history' in window)) return
+    if (!('history' in window)) return
     this._menu = menu
     this._scroll = this._scroll.bind(this)
     window.addEventListener('scroll', debounce(this._scroll, 100))
-    if(location.hash) {
+    if (location.hash) {
       scrollTo(location.hash, 500)
     }
   }
 
   _scroll() {
     const id = this._menu.followMark()
-    history.replaceState(null, null, `#${id}`)
+    if (id === 'top') {
+      history.replaceState(null, null, '.')
+    } else {
+      history.replaceState(null, null, `#${id}`)
+    }
   }
 }
 
