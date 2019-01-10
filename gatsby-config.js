4c8c1cf7ef09126e7bb8d304065724c041837fe2
sunya9 <mugen.xyz@gmail.com>
2019-01-10 12:06:51 +0900

    Fix configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
+const pkg = require('./package')
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: pkg.name,
+    author: `_X_y_z_`,
+    description: pkg.description,
+    siteUrl: `https://blog.unsweets.net`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `_X_y_z_`,
     },
   },
   plugins: [
@@ -21,6 +23,13 @@ module.exports = {
       options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages`,
+        name: `pages`,
       },
     },
     {
@@ -57,13 +66,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: pkg.description,
+        short_name: pkg.name,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#569d90`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
@@ -73,6 +81,6 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
-    },
+    }
   ],
 }
