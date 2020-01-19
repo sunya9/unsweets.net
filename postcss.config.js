be885d034d40e96258a8c3ab109cf206facbe3a2
sunya9 <mugen.xyz@gmail.com>
2020-01-19 23:47:37 +0900

    Fix css syntax

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,13 @@ const prod = process.env.NODE_ENV === 'production'
 const config = {
   plugins: [
     require('postcss-import')(),
-    require('postcss-preset-env')()
+    require('postcss-preset-env')({
+      stage: 0,
+      features: {
+        'color-mod-function': true
+      },
+    }),
+    require('postcss-color-gray')()
   ]
 }
 
