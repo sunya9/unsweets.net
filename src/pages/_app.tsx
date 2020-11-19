77b378511527ed8d8e1a60441f60a754f62aba26
sunya9 <mugen.xyz@gmail.com>
2020-11-20 00:02:04 +0900

    feat: define default title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,29 @@
 import { AppProps } from "next/app";
 import { ConfigProvider } from "../components/ConfigProvider";
+import Head from "next/head";
+import { useConfig } from "../hooks/useConfig";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ConfigProvider>
-      <Component {...pageProps} />
+      <App>
+        <Component {...pageProps} />
+      </App>
     </ConfigProvider>
   );
 }
+
+const App: React.FC = ({ children }) => {
+  const config = useConfig();
+  return (
+    <>
+      <Head>
+        <title>{config.title()}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {children}
+    </>
+  );
+};
 
 export default MyApp;
