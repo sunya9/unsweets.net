3b2f9b5d21b33c5bf36751abdb234b0b1becb57c
sunya9 <mugen.xyz@gmail.com>
2020-11-20 08:19:37 +0900

    feat: show entry list grouped by year on entry page

--- src/pages/archives.tsx
+++ src/pages/archives.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import Head from "next/head";
 import { GetStaticProps } from "next";
 import { Entry } from "../lib/entry";
@@ -10,6 +11,18 @@ interface Props {
 }
 export default function Home(props: Props) {
   const config = useConfig();
+  const entriesGroupedByYear = props.entries.reduce<{
+    [key: number]: Omit<Entry, "body">[];
+  }>((memo, entry) => {
+    const year = new Date(entry.date).getFullYear();
+    return {
+      ...memo,
+      [year]: (memo[year] || []).concat(entry),
+    };
+  }, {});
+  const years = Object.keys(entriesGroupedByYear)
+    .map((year) => +year)
+    .sort((a, b) => b - a);
   return (
     <div>
       <Head>
@@ -18,7 +31,12 @@ export default function Home(props: Props) {
 
       <main>
         <h1>Archives</h1>
-        <EntryList entries={props.entries} />
+        {years.map((year) => (
+          <React.Fragment key={year}>
+            <h2>{year}</h2>
+            <EntryList entries={entriesGroupedByYear[year]} omitYear />
+          </React.Fragment>
+        ))}
       </main>
     </div>
   );
