4b7a3c3daba2ef613f1b88cbdc5a6b1458bb1169
sunya9 <mugen.xyz@gmail.com>
2020-11-22 07:08:02 +0900

    feat: add react-medium-image-zoom

--- src/components/EntryView.tsx
+++ src/components/EntryView.tsx
@@ -6,6 +6,8 @@ import { AbsDate } from "./AbsDate";
 import { NextLinkIfInternalAnchor } from "./NextLinkIfAnchor";
 import { Page } from "../lib/page";
 import dynamic from "next/dynamic";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 const Pre = dynamic<unknown>(
   import("../components/Pre").then((res) => res.Pre)
@@ -15,9 +17,23 @@ interface Props {
   entry: Page;
 }
 
-const Img = ({ src, ...rest }: ImgHTMLAttributes<HTMLImageElement>) => (
-  <img src={src} {...rest} className="border shadow-lg mx-auto" />
-);
+const Img = ({ src, ...rest }: ImgHTMLAttributes<HTMLImageElement>) => {
+  const nonNullableSrc = src || "";
+  const fixedSrc = nonNullableSrc.startsWith("/")
+    ? nonNullableSrc
+    : nonNullableSrc.replace(/^\.\.\/\.\.\/public/, "");
+  return (
+    <span className="block text-center my-8">
+      <Zoom wrapElement="span" zoomMargin={16}>
+        <img
+          src={fixedSrc}
+          {...rest}
+          className="border shadow-lg max-h-64 my-0"
+        />
+      </Zoom>
+    </span>
+  );
+};
 
 export const EntryView = ({ entry }: Props) => {
   const config = useConfig();
