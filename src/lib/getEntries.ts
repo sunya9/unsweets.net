28885f8356f7468c87c3157af4194fa52a0900ce
sunya9 <mugen.xyz@gmail.com>
2020-11-22 04:45:42 +0900

    fix: getEntries always get full entry

--- src/lib/getEntries.ts
+++ src/lib/getEntries.ts
@@ -6,9 +6,7 @@ import * as path from "path";
 
 const { blogDir } = config;
 
-export const getEntries = async (
-  limit?: number
-): Promise<Omit<Entry, "body">[]> => {
+export const getEntries = async (limit?: number): Promise<Entry[]> => {
   const slugs = await fs.readdir(blogDir);
   const entryPromises = slugs.map((filename) =>
     getEntry(path.basename(filename, ".md"))
@@ -16,7 +14,6 @@ export const getEntries = async (
   return Promise.all(entryPromises).then((entries) =>
     entries
       .slice()
-      .map(({ date, slug, title }) => ({ date, slug, title }))
       .sort((a, b) => b.date - a.date)
       .slice(0, limit || entries.length)
   );
