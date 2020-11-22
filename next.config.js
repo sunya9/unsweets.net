ad94ba18891a044c4c0f881eb7d7d46f93c1b249
sunya9 <mugen.xyz@gmail.com>
2020-11-23 01:46:01 +0900

    fix: move redirects configuration fomr next.config.js to vercel.json

--- next.config.js
+++ next.config.js
@@ -1,35 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withOffline = require("next-offline");
-const { promises: fs } = require("fs");
-const path = require("path");
-const matter = require("gray-matter");
+
 module.exports = withOffline({
   trailingSlash: true,
-  async redirects() {
-    // workaround
-    const blogDir = path.resolve(__dirname, "content", "blog");
-    const entryNames = await fs.readdir(blogDir);
-    const entryPathPromises = entryNames.map(async (name) => {
-      const mdFilename = path.resolve(blogDir, name);
-      const md = await fs.readFile(mdFilename, "utf-8");
-      const { data } = matter(md);
-      const date = new Date(data.date);
-      const year = zeroPad(date.getFullYear());
-      const month = zeroPad(date.getMonth() + 1);
-      const slug = path.basename(name, ".md");
-      return {
-        oldPath: `/${year}/${month}/${slug}/`,
-        newPath: `/entries/${slug}/`,
-      };
-    });
-    const entryPaths = await Promise.all(entryPathPromises);
-    const redirectOldEntryPaths = entryPaths.map((entryPath) => ({
-      source: entryPath.oldPath,
-      destination: entryPath.newPath,
-      permanent: true,
-    }));
-    return [...redirectOldEntryPaths];
-  },
 });
-
-const zeroPad = (num) => num.toString().padStart(2, "0");
