57949e03e62ff940417d8d463d423b9915a2e4a3
sunya9 <mugen.xyz@gmail.com>
2021-05-22 10:57:18 +0900

    refactor: define custom app component as function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "next/app";
+import { AppProps } from "next/app";
 import Head from "next/head";
 import "../css/global.scss";
 import smoothscroll from "smoothscroll-polyfill";
@@ -8,21 +8,17 @@ if (process.browser) {
   smoothscroll.polyfill();
 }
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1.0,minimum-scale=0.25"
-          />
-          <title>&lt;unsweets/&gt;</title>
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
-  }
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=0.25"
+        />
+        <title>&lt;unsweets/&gt;</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp;
