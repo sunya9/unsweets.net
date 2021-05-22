22408fcf80f5113d328accf00be3f36a9f4fac33
sunya9 <mugen.xyz@gmail.com>
2021-05-22 11:14:00 +0900

    feat: add canonical

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import "../css/global.scss";
 import smoothscroll from "smoothscroll-polyfill";
+import { useRouter } from "next/router";
 
 if (process.browser) {
   smoothscroll.polyfill();
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const url = `${process.env.NEXT_PUBLIC_VERCEL_URL || ""}${router.route}`;
   return (
     <>
       <Head>
@@ -17,6 +20,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width,initial-scale=1.0,minimum-scale=0.25"
         />
         <meta name="theme-color" content="#e74c3c" />
+        <link rel="canonical" href={url} />
         <title>&lt;unsweets/&gt;</title>
       </Head>
       <Component {...pageProps} />
