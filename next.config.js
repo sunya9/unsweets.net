9dbe67212ccaa9c22deb55a79c7ffbb2baeb923c
sunya9 <mugen.xyz@gmail.com>
2021-05-22 11:32:23 +0900

    feat: use webpack5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withImages = require("next-images");
 
 module.exports = withImages({
+  future: {
+    webpack5: true,
+  },
   cssLoaderOptions: {
     localIdentName: "[name]-[local]-[hash:base64:5]",
   },
