da9d7085ab852cb4e6ea5abf235228cc3cc35e8a
sunya9 <mugen.xyz@gmail.com>
2021-07-05 07:01:17 +0900

    fix: webpack5 declaration from future to top level

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 const withImages = require("next-images");
 
 module.exports = withImages({
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   cssLoaderOptions: {
     localIdentName: "[name]-[local]-[hash:base64:5]",
   },
