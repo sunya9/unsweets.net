11993aa7b8beff4f9bc7a6652e5f7bc0984b64e7
sunya9 <mugen.xyz@gmail.com>
2022-03-15 07:49:20 +0900

    fix: google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 const MyDocument = () => {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Rubik&display=optional"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
