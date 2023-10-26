0e70f15e6425b8c47007a177746db8ad6111ba87
sunya9 <mugen.xyz@gmail.com>
2023-10-26 11:51:13 +0900

    fix: move server configuration from vercel.json to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,28 @@
  **/
 const config = {
   reactStrictMode: true,
+  redirects: async () => [
+    {
+      source: "/:year(\\d{4,})/:month(\\d{2,})/:slug",
+      destination: "/entries/:slug",
+      permanent: true,
+    },
+  ],
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "X-Frame-Options",
+          value: "SAMEORIGIN",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "no-referrer-when-downgrade",
+        },
+      ],
+    },
+  ],
   webpack: (webpackConfig) => {
     // https://github.com/vercel/next.js/issues/41961
     webpackConfig.resolve.extensionAlias = {
