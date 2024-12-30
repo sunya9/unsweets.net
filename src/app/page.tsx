341d9884e71970f3d641c6727c9ef983a8b985fa
xyz <mugen.xyz@gmail.com>
2024-12-31 01:21:50 +0900

    chore: remove unnecessary backslash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
   return (
     <>
       <section>
-        <p>unsweets.netは\_X_y_z\_が技術メモなどを綴っています。</p>
+        <p>unsweets.netは_X_y_z_が技術メモなどを綴っています。</p>
 
         <p>主にNode.js関連の話題を取り扱っています。</p>
       </section>
